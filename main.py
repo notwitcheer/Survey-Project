c9e428ee389d32421bfddf383daa9cdaa36d81f4
witcheer <witcheer.eth@gmail.com>
2025-07-31 09:39:19 +0200

    v3 with OOP

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # survey creation.
 
-
 def ask_numerical_answer(min, max):
     answer_str = input(f"your answer (between {str(min)} and {str(max)}): ")
     try:
@@ -14,38 +13,51 @@ def ask_numerical_answer(min, max):
     return ask_numerical_answer(min, max)
 
 
-def ask_question(q):
-    choice = q[1]
-    good_answer = q[2]
-    print("question: " + q[0])
+
+class Quiz:
+    def __init__(self, questions):
+        self.questions = questions
+
     
-    for i in range (len(choice)):
-        print(i+1,"-", choice[i])
-
-    result_correct_answer = False
-    answer_int = ask_numerical_answer(1, len(choice))
-    if choice[answer_int-1].lower() == good_answer.lower():
-        print("\nCONGRATULATIONS.\n")
-        result_correct_answer =  True
-    else:
-        print("\nWRONG ANSWER.\n")
-    return result_correct_answer
+    def start_quiz(self):
+        score = 0
+        for question in self.questions:
+            if question.ask():
+                score += 1
+        print(f"final score: {score} on {len(self.questions)}.")
+        return score
 
 
-def start_survey(survey):
-    score = 0
-    for q in survey:
-        if ask_question(q):
-            score += 1
-    print(f"final score: {score} on {len(survey)}.")
+
+class Question:
+    def __init__(self, title, options, answer):
+        self.title = title
+        self.options = options
+        self.answer = answer
+
+
+    def ask(self):
+        choice = self.options
+        good_answer = self.answer
+        print("QUESTION: " + self.title)
         
+        for i in range(len(choice)):
+            print(i+1, "-", choice[i])
+
+        result_correct_answer = False
+        answer_int = ask_numerical_answer(1, len(choice))
+        if choice[answer_int-1].lower() == good_answer.lower():
+            print("\nCONGRATULATIONS.\n")
+            result_correct_answer = True
+        else:
+            print("\nWRONG ANSWER.\n")
+        return result_correct_answer
 
 
-survey = (
-    ("what is the capital of France?", ("Marseille", "Nice", "Paris", "Nantes"), "Paris"),
-    ("what is the capital of Italie?", ("Milan", "Rome", "Turin", "Naples"), "Rome"),
-    ("what is the capital of Spain?", ("Madrid", "Barcelona", "Salamanqua", "Torrente"), "Madrid"),
-    ("what is the capital of Russia?", ("Moscow", "Novoss", "Petersbourg", "Lekaterin"), "Moscow"),
-)
+Quiz([
+    Question("what is the capital of France?", ("Marseille", "Nice", "Paris", "Nantes"), "Paris"),
+    Question("what is the capital of Italie?", ("Milan", "Rome", "Turin", "Naples"), "Rome"),
+    Question("what is the capital of Spain?", ("Madrid", "Barcelona", "Salamanqua", "Torrente"), "Madrid"),
+    Question("what is the capital of Russia?", ("Moscow", "Novoss", "Petersbourg", "Lekaterin"), "Moscow"),
+]).start_quiz()
 
-start_survey(survey)
